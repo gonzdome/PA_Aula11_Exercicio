4d29df973fd134626d70ded297a72eeafa73a340
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-25 20:41:38 -0300

    Add project files.

---
+++ Aula11_Exercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahr, celsius;
+
+            Console.WriteLine("Informe a temperatura em Fahrenheit: ");
+            fahr = double.Parse(Console.ReadLine());
+
+            celsius = (fahr - 32)/1.8;
+
+            Console.WriteLine("A temperatura equivalente em Celcius é de " + celsius + "°!");
+
+            Console.ReadKey();
+        }
+    }
+}
